8fb831f16cc5dd3f06b822f1d0ceab697a74f26c
shintaroasuzuki <shinbo.dorapion19971221@gmail.com>
2023-05-15 20:30:50 +0900

    fix: :lady_beetle: テストコードの追加

--- README.md
+++ README.md
@@ -1,12 +1,10 @@
 # chatgpt-review
 
-## prerequisites
+## prerequisites(optional)
 
 ```
 go install github.com/daixiang0/gci@latest
 brew install golangci-lint
-brew install k1LoW/tap/octocov
-yarn install
 ```
 
 Change "Read repository contents permission" to "Read and write permissions" in Repository Settings > Actions > General > Workflow permissions.
--- main_test.go
+++ main_test.go
@@ -11,3 +11,25 @@ func TestGitClone(t *testing.T) {
 		t.Errorf("expected error, but got nil")
 	}
 }
+
+func TestCdRepository(t *testing.T) {
+	// 不正な repo を渡したときにエラーが発生することを確認する
+	err := CdRepository("")
+	if err == nil {
+		t.Errorf("expected error, but got nil")
+	}
+}
+
+func TestSplitRepositoryName(t *testing.T) {
+	// 不正な repository を渡したときにエラーが発生することを確認する
+	_, _, err := SplitRepositoryName("a/b/c")
+	if err == nil {
+		t.Errorf("expected error, but got nil")
+	}
+
+	// 正しい repository を渡したときにエラーが発生しないことを確認する
+	_, _, err = SplitRepositoryName("a/b")
+	if err != nil {
+		t.Errorf("expected nil, but got %v", err)
+	}
+}
